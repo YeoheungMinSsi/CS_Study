630d401e5ab677765b25101e0a1366cd5271be88
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2025-12-03 02:15:54 +0900

    복합패턴 1 추가

---
+++ DesignPattern/CompositePattern/homework/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CompositePattern.homework;
+
+public class Program
+{
+    public static void Main()
+    {
+        // 태그 값
+        var divNode = new ElementNode("div");
+        var pNode = new ElementNode("p");
+        var spanNode = new ElementNode("span");
+        
+        // 텍스트
+        var helloText = new TextNode("Hello");
+        var worldText = new TextNode("World");
+        
+        // 연결
+        spanNode.AddChild(worldText);
+        // p 노드의 자식
+        pNode.AddChild(helloText);
+        pNode.AddChild(spanNode);
+        
+        divNode.AddChild(pNode);
+        // div - p - hello - span - world 
+        string finalHtml = divNode.Render();
+        Console.WriteLine(finalHtml);
+
+    }
+}
---
+++ DesignPattern/CompositePattern/homework/TextNode.cs
@@ -0,0 +1,32 @@
+﻿namespace CompositePattern.homework;
+
+// note: 순수 텍스트를 표현
+public class TextNode: INode
+{
+    // 텍스트의 불변성 추가
+    private readonly string _text;
+
+    public TextNode(string text)
+    {
+        _text = text;
+    }
+
+    public void AddChild(INode node)
+    {
+        throw new NotSupportedException("TextNode는 자식 노드를 가질 수 없습니다.");
+    }
+    public void RemoveChild(INode node)
+    {
+        throw new NotSupportedException("TextNode는 자식 노드를 제거할 수 없습니다.");
+    }
+    
+    public string Render()
+    {
+        return this._text;
+    }
+
+    public string ToJson()
+    {
+        return this.Render();
+    }
+}
